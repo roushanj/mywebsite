92f6e6855b0b6d10dc2ef2b45f0042c289be65b0
Roushan <roushan.jha0709@gmail.com>
2017-08-12 13:13:11 +0530

    added server file

---
+++ server.js
@@ -0,0 +1,26 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var exphbs = require('express-handlebars');
+var app= express();
+var main = require('./router/main');
+
+
+app.set('views',path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout : null }));
+app.set('view engine', 'handlebars');
+
+
+app.use(express.static(path.join(__dirname, ('public'))));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/',main);
+
+app.listen(process.env.PORT || 3000, function() {
+
+	console.log('server is listening');
+});
