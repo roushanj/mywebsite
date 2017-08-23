3356997e3b9e7b57264591e27346618dbf124cbd
Roushan <roushan.jha0709@gmail.com>
2017-08-23 12:12:53 +0530

    created new tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
-gulp.task('renderhandlebars', function() {
-	
-	gulp.src('src/*.handlebars')
-       .pipe(gulp.dest('dist'));
-});
 
 gulp.task('default', function() {
+
+   gulp.src('views/*.handlebars')
+       .pipe(gulp.dest('dist'));
+
+   gulp.src('public/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
+
+   gulp.src('public/js/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/js'));
+
+
 	return console.log('gulp in action');
 });
+
